3253588caef9e1024135901a28dc9f259b370b26
Benjamin Crouzier <benjamin@follow-apps.com>
2014-06-03 21:46:23 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Perso::Application.config.secret_key_base = 'dd9ef21f833246eba77150dbbb766ff7b3c0f7b3cdc31beed7a1f5df6aa9ad1cc048a43ed506bb7cf83b27725d5ba4c58a75550735dfaee05cf4a97aee47e836'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Perso::Application.config.session_store :cookie_store, key: '_perso_session'
